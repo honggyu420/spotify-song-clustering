9402e9af871cc1d0b00e278111c4f2138ec9d750
Hong-Gyu <hgl225@nyu.edu>
2020-04-13 23:49:22 -0400

    initial commit

---
+++ authenticate.py
@@ -0,0 +1,24 @@
+import sys
+import spotipy
+import spotipy.util as util
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+scope = 'user-library-read'
+username = config['SPOTIFY']['username']
+client_id=config['SPOTIFY']['client_id']
+client_secret=config['SPOTIFY']['client_secret']
+redirect_uri=config['SPOTIFY']['redirect_uri']
+
+token = util.prompt_for_user_token(username, scope,
+                           client_id=client_id,
+                           client_secret=client_secret,
+                           redirect_uri=redirect_uri)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    print("authenticated")
+else:
+    print("No token")
